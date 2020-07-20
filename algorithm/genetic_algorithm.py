cb6bb25b877962b027fb6e96ae28fd47a6593e69
Kehua Chen <48197101+ckjzsa@users.noreply.github.com>
2020-07-20 13:38:31 +0800

    Add genetic algorithm

---
+++ algorithm/genetic_algorithm.py
@@ -0,0 +1,150 @@
+import random
+
+
+class NaiveGeneticAlgorithm:
+    def __init__(self, size, dimension, sol_range, chrom_size, cp, mp, gen_max, fitness_function):
+        # 种群信息
+        self.individuals = []  # 个体信息
+        self.fitness = []  # 适应度
+        self.select_pro = []  # 选择概率
+        self.new_individuals = []  # 新一代个体
+        self.dimension = dimension  # 变量维度
+        self.sol_range = sol_range
+        self.interval = sol_range[1] - sol_range[0]  # 求解范围
+        self.elitist = {'chromosome': [0 for _ in range(self.dimension)], 'fitness': float('-inf'), 'age': 0}
+
+        # 超参数
+        self.size = size  # 种群规模
+        self.chrom_size = chrom_size  # 染色体长度
+        self.cross_over = cp  # 交叉变异概率
+        self.mutation = mp  # 突变概率
+        self.cur_generation = 0  # 当前代数
+        self.generation_max = gen_max
+
+        # 初始化种群
+        binary_length = 2 ** self.chrom_size - 1  # 二进制长度
+        for i in range(self.size):
+            self.individuals.append([random.randint(0, binary_length) for _ in range(self.dimension)])
+            self.new_individuals.append([0 for _ in range(self.dimension)])
+            self.fitness.append(float('-inf'))
+            self.select_pro.append(0.0)
+
+        # 评价函数
+        self.fitness_function = fitness_function
+
+    def decode(self, chromosome):   # 解码后的十进制数
+        binary_num = 2 ** self.chrom_size - 1
+        decode_num = self.sol_range[0] + chromosome * self.interval / binary_num
+
+        return decode_num
+
+    def fitness_sol(self, chroms):
+        variable = [self.decode(chrom) for chrom in chroms]
+        x = variable[0]
+        y = variable[1]
+        res = self.fitness_function(x, y)
+
+        return res
+
+    def evaluate(self):  # 进行打分
+        sp = self.select_pro
+        for i in range(self.size):
+            self.fitness[i] = -1 / self.fitness_sol(self.individuals[i])
+        fitness_sum = sum(self.fitness)
+        for i in range(self.size):
+            sp[i] = self.fitness[i] / float(fitness_sum)
+        for i in range(1, self.size):
+            sp[i] = sp[i] + sp[i-1]  # 概率累加为1
+
+    def select(self):  # 选择染色体
+        t = random.random()
+        i = 0
+        for p in self.select_pro:
+            if p > t:
+                break
+            i += 1
+
+        return i
+
+    def cross(self, chrom1, chrom2):
+        p = random.random()
+        binary_length = 2 ** self.chrom_size - 1
+        if chrom1 != chrom2 and p < self.cross_over:  # 进行交叉变异
+            cross_point = random.randint(1, self.chrom_size-1)  # 交叉点
+            mask = binary_length << cross_point
+            r1, r2 = chrom1 & mask, chrom2 & mask
+            mask = binary_length >> (self.chrom_size - cross_point)
+            l1, l2 = chrom1 & mask, chrom2 & mask
+            chrom1, chrom2 = r1 + l2, r2 + l1
+
+        return [chrom1, chrom2]
+
+    def mutate(self, chrom):
+        p = random.random()
+        if p < self.mutation:
+            mutate_point = random.randint(1, self.chrom_size)
+            mask1 = 1 << (mutate_point - 1)
+            mask2 = chrom & mask1
+            if mask2 > 0:
+                chrom = chrom & (~mask2)
+            else:
+                chrom = chrom ^ mask1
+
+        return chrom
+
+    def save_elitist(self):
+        j = -1
+        for i in range(self.size):
+            if self.elitist['fitness'] < self.fitness[i]:
+                j = i
+                self.elitist['fitness'] = self.fitness[i]
+
+        if j >= 0:
+            for i in range(self.dimension):
+                self.elitist['chromosome'][i] = self.individuals[j][i]
+
+            # self.elitist['age'] = self.cur_generation
+
+    def evolve(self):
+        self.evaluate()
+        i = 0
+        while True:
+            individual1 = self.select()
+            individual2 = self.select()
+            ind1_set = [None] * self.dimension
+            ind2_set = [None] * self.dimension
+
+            # 交叉
+            for dim in range(self.dimension):
+                ind1_set[dim] = self.individuals[individual1][dim]
+                ind2_set[dim] = self.individuals[individual2][dim]
+
+                [ind1_set[dim], ind2_set[dim]] = self.cross(ind1_set[dim], ind2_set[dim])
+
+                # 变异
+                ind1_set[dim] = self.mutate(ind1_set[dim])
+                ind2_set[dim] = self.mutate(ind2_set[dim])
+
+                self.new_individuals[i][dim] = ind1_set[dim]
+                self.new_individuals[i+1][dim] = ind2_set[dim]
+
+            i += 2
+            if i >= self.size:
+                break
+
+        self.save_elitist()
+
+        for i in range(self.size):
+            for j in range(self.dimension):
+                self.individuals[i][j] = self.new_individuals[i][j]
+
+    def solver(self):
+        for i in range(self.generation_max):
+            self.evolve()
+            best_sol = []
+            print("Epoch {}, maximum fitness is {}, mean fitness is {}".format(i, max(self.fitness), sum(self.fitness) / self.size))
+            best_index = self.fitness.index(max(self.fitness))
+            for j in self.individuals[best_index]:
+                best_sol.append(self.decode(j))
+            print("Best solution is {}".format(best_sol))
+
--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 from algorithm.problem_generation import Problem
 from algorithm.branch_and_bound import BranchAndBound
+from algorithm.genetic_algorithm import NaiveGeneticAlgorithm
+import math
+
+
+def func(x, y):
+    return -(x-1)**2 - (y-2)**2
 
 
 def main():
-    pb = Problem([40, 90], max_problem=True)
-    # 约束时保证维度一致
-    pb.add_constraint([9, 7], '<=', 56)
-    pb.add_constraint([7, 20], '<=', 70)
+    # # 生成cost向量
+    # pb = Problem([40, 90], max_problem=True)
+    # # 约束时保证维度一致
+    # pb.add_constraint([9, 7], '<=', 56)
+    # pb.add_constraint([7, 20], '<=', 70)
+    #
+    # # 普通线性规划
+    # # solver会打印并返回结果
+    # # pb.solver()
+    #
+    # # 整型规划
+    # integer_index = [1]
+    # c, a, b, symbol = pb.origin()
+    # pb_ilp = BranchAndBound(integer_index, c, a, b, symbol)
+    # result = pb_ilp.solver()
+    # for i, j in zip(result[0], result[1]):
+    #     print('x_{} is {}'.format(i, j))
+    #
+    # print('最大值为{}'.format(result[2]))
 
-    # 普通线性规划
-    # solver会打印并返回结果
-    # pb.solver()
-
-    # 整型规划
-    integer_index = [1]
-    c, a, b, symbol = pb.origin()
-    pb_ilp = BranchAndBound(integer_index, c, a, b, symbol)
-    result = pb_ilp.solver()
-    for i, j in zip(result[0], result[1]):
-        print('x_{} is {}'.format(i, j))
-
-    print('最大值为{}'.format(result[2]))
+    # 遗传算法
+    pb = NaiveGeneticAlgorithm(size=50, dimension=2, sol_range=[-5, 5],
+                               chrom_size=25, cp=0.8, mp=0.1, gen_max=500, fitness_function=func)
+    pb.solver()
 
 
 if __name__ == '__main__':
